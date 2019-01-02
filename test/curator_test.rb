ef3d3905510218bc27fb1d85f0433b7c4e809ce6
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-02 13:40:51 -0700

    Added photos to setup and test for adding photos

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -6,6 +6,20 @@ require './lib/curator'
 class CuratorTest < Minitest::Test
 
   def setup
+    @photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+
+    @photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+
     @curator = Curator.new
   end
 
@@ -19,6 +33,13 @@ class CuratorTest < Minitest::Test
 
   def test_it_starts_with_no_photographs
     assert_equal [], @curator.photographs
+  end
+
+  def test_it_can_add_photos
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
   end
 
 end
