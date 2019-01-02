edf77218f1c22c8ddb01a4dea5a2bef327a1d949
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-02 13:10:02 -0700

    Added set up for photograph test

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,14 @@ require './lib/photograph'
 
 class PhotographTest < Minitest::Test
 
+  def setup
+    @attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: 4,
+      year: 1954
+
+    @photograph = Photograph.new(@attributes)
+  end
+
 end
