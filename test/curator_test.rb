6f1d34b7b63f4f622140cf5f734bff70e5e0533c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-02 13:53:54 -0700

    Added find by id methods for artists and photographs

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,40 +1,48 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/photograph'
+require './lib/artist'
 require './lib/curator'
 
 
 class CuratorTest < Minitest::Test
 
   def setup
-    @photo_1 = {
+    @photo_1_attributes = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "1",
       year: "1954"
     }
 
-    @photo_2 = {
+    @photo_2_attributes = {
       id: "2",
       name: "Moonrise, Hernandez",
       artist_id: "2",
       year: "1941"
     }
 
-    @artist_1 = {
-      id: "1"
+    @artist_1_attributes = {
+      id: "1",
       name: "Henri Cartier-Bresson",
       born: "1908",
       died: "2004",
       country: "France"
     }
 
-    @artist_2 = {
+    @artist_2_attributes = {
       id: "2",
       name: "Ansel Adams",
       born: "1902",
       died: "1984",
       country: "United States"
     }
+
+    @photo_1 = Photograph.new(@photo_1_attributes)
+    @photo_2 = Photograph.new(@photo_2_attributes)
+
+    @artist_1 = Artist.new(@artist_1_attributes)
+    @artist_2 = Artist.new(@artist_2_attributes)
 
     @curator = Curator.new
   end
@@ -63,6 +71,20 @@ class CuratorTest < Minitest::Test
     @curator.add_artist(@artist_2)
 
     assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_it_can_find_artists_by_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+  end
+
+  def test_it_can_find_photos_by_id
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal @photo_2, @curator.find_photograph_by_id("2")
   end
 
 end
